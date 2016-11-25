f28481dcd92d08140b2aed465b1bb9d98b31de35
yejune <kwon@yejune.com>
2016-11-25 15:00:37 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# tree-view-project package
+
+A short description of your package.
+
+![A screenshot of your package](https://github.com/yejune/tree-view-project/blob/master/screenshot.gif?raw=true)
---
+++ spec/tree-view-project-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TreeViewProjectView from '../lib/tree-view-project-view';
+
+describe('TreeViewProjectView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
